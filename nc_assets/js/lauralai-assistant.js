a3c60d4bd9c12423641243f6a199169bdaf255a1
250proud admin <360podcast@gmail.com>
2026-06-18 17:03:22 -0500

    Update chat button to use ASK LauralAI text instead of icon

--- nc_assets/js/lauralai-assistant.js
+++ nc_assets/js/lauralai-assistant.js
@@ -27,7 +27,8 @@
             </div>
             
             <button class="lauralai-chat-button" id="lauralaiChatBtn" title="Chat with LauralAI">
-                <svg viewBox="0 0 24 24"><path d="M20 2H4c-1.1 0-2 .9-2 2v18l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm0 14H6l-2 2V4h16v12z"/></svg>
+                <span class="lauralai-btn-top">ASK</span>
+                <span class="lauralai-btn-bottom">LauralAI</span>
             </button>
         </div>
     `;
